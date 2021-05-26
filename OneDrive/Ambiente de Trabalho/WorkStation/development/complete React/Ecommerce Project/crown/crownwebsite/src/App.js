61f263e74523e4b3ea462a668c9f9b4d2c836fca
KennedyNvsf <kennedynvsf@gmail.com>
2021-05-26 12:10:09 +0100

    Created the creat-react-app and installed node-sass

---
+++ OneDrive/Ambiente de Trabalho/WorkStation/development/complete React/Ecommerce Project/crown/crownwebsite/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import HomePage from "./homePage.component";
+import "./homePage.styles.scss";
+
+function App() {
+
+  return (
+    <div>
+
+      <HomePage/>
+    
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ OneDrive/Ambiente de Trabalho/WorkStation/development/complete React/Ecommerce Project/crown/crownwebsite/src/homePage.component.jsx
@@ -0,0 +1,76 @@
+
+
+//importing React 
+import React from "react";
+
+
+
+//Creating component
+//functional component
+
+const HomePage = () => (
+
+    <div className = "homePage">
+
+        <div className="directoryMenu">
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">HATS</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">JACKETS</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">SNEAKERS</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">WOMENS</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">MEN</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className ="menuItem">
+                
+                <div className ="content">
+
+                    <h1 className ="title">HATS</h1>
+                    <span className ="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+);
+
+
+export default HomePage;
