702318f5e11142c97223055cd52b644073a20b68
KennedyNvsf <kennedynvsf@gmail.com>
2021-07-19 10:48:29 +0100

    initial setup

--- OneDrive/Ambiente de Trabalho/WorkStation/development/sonny/Gmail/gmail-clone/src/App.js
+++ OneDrive/Ambiente de Trabalho/WorkStation/development/sonny/Gmail/gmail-clone/src/App.js
@@ -1,12 +1,75 @@
 import React from 'react';
+import "./styles/app.scss";//styling
+
+
+//REACT ROUTER DOM
+
+import { BrowserRouter as Router, Route,  Switch } from "react-router-dom";  
+
+//REDUX
+
+import {useSelector} from "react-redux";
+import { selectsendMessageIsOpen } from './features/mail.slice';
+// import { selectUser } from './features/user.slice';
+
+
+
+//components
+// import LoginPage from "./pages/loginPage/loginPage.component";
+import Header from './components/header/header.component';
+import SideBar from './components/sidebar/sidebar.component';
+import Mail from './components/mail_/mail.component';
+import EmailList from './components/emailList_/email_list.component';
+import SendMail from './components/sendmail/sendMail.component';
+
+
 
 
 function App() {
+
+  const sendMessageIsOpen = useSelector(selectsendMessageIsOpen);
+  // const user = useSelector(selectUser)
+  
+
   return (
-    <div className="App">
-     
-    </div>
-  );
-}
+
+  
+
+    <Router>
+
+              
+              <div className="App">
+
+            
+              <Header/>
+
+                  <div className ="appBody">
+
+                    <SideBar/>
+
+                    <Switch>
+
+                        <Route exact path="/">
+                          <EmailList/>
+                        </Route>
+
+                        <Route  path="/mail">
+                          <Mail/>
+                        </Route>
+
+                    </Switch>
+
+                  </div>
+
+                {sendMessageIsOpen && <SendMail/> }
+                
+            
+            </div>
+
+          
+      </Router>
+   
+  )
+};
 
 export default App;
--- OneDrive/Ambiente de Trabalho/WorkStation/development/sonny/Gmail/gmail-clone/src/app/store.js
+++ OneDrive/Ambiente de Trabalho/WorkStation/development/sonny/Gmail/gmail-clone/src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import mailReducer from "../features/mail.slice";
+import userReducer from "../features/user.slice"
 
 
 export const store = configureStore({
   reducer: {
     
+    mail: mailReducer,
+    user: userReducer,
+
   },
 });
